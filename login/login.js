66e1265a5cc64c50b5db873e9394be4a2b4b3d24
Pietro <79109577+Piterprot@users.noreply.github.com>
2024-04-08 23:07:02 +0200

    Aggiunta logica
    Aggiunta logica per identificare la pagina e per i calcoli. da implementare la scrittura sui file json

--- login/login.js
+++ login/login.js
@@ -21,11 +21,8 @@ function Login(){
       case 1111: 
         if(password == classe[0].password){
           console.log("Sono nel case 1111")
-         let numeroRiferimento = FunRiferimento(); 
-         numeroRiferimento = numeroRiferimento-9999;
-         console.log(numeroRiferimento);
          window.location.href = "../pag_loggate/conc.html";
-         
+         Sovrascrivo();
         }
         else{
           alert("Nome utente o password errati");
@@ -47,22 +44,60 @@ function Login(){
 
 function Sovrascrivo(){
 
-    console.log(riferimento)
+  let riferimento = 9999;
+  let riferimentoTestuale = document.getElementById("TextRiferimento").innerHTML; 
+  switch(riferimentoTestuale){
+
+    case "CONCESSIONARIO":
+      riferimento = 0; 
+      break;
+  }
+
     let requestURL = 'datiloggati.json';
-        let request = new XMLHttpRequest();
-        request.open('GET', requestURL);
-        request.responseType = 'json';
-        request.send();
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
 
     request.onload = function (){
     const db = request.response;
-    document.getElementById("FattEmesseConc").innerHTML = db[0].FattEmesse  
-    document.getElementById("FattTotale").innerHTML = db[0].totFatturato  
-    }
-}  
+    document.getElementById("FattEmesse").innerHTML = db[riferimento].FattEmesse  
+    document.getElementById("FattTotale").innerHTML = db[riferimento].totFatturato
+  
+     let FattEmesseIniziali = db[riferimento].FattEmesse;
+      let FattEmesseNuove = parseInt(document.getElementById("fattEmesse").value);
+      let FattEmesseTotali = FattEmesseIniziali + FattEmesseNuove;
+    
+      let totFatturatoIniziale = db[riferimento].totFatturato;
+      let totFatturatoNuovo = parseInt(document.getElementById("Importo").value);
+      let totFatturatoTotale = totFatturatoIniziale + totFatturatoNuovo;
 
-function FunRiferimento(){
-  let riferimento = 9999; 
-  console.log(riferimento)
-  return riferimento;
-}
+      const newData = JSON.stringify(db, null, 2);
+      
+      
+        let fileWriter = new XMLHttpRequest();
+        fileWriter.open = ('PUT', 'datiloggati.json');
+        fileWriter.setRequestHeader('Content-Type', 'application/json');
+        fileWriter.onload = function(){
+      
+       }
+      fileWriter.send(newData);
+  }  
+
+    let DataInizio = document.getElementById("DataInizio").value;
+    let DataFine = document.getElementById("DataFine").value;
+
+    /*
+    if(DataFine < DataInizio){
+      alert("Le date non sono state inserite correttamente");
+      return;
+    }
+    if(DataInizio == "" || DataFine == ""){
+      alert("Inserire le date");
+      return;
+    } */
+}   
+
+
+
